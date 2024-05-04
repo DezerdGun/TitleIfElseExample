f0f84fe1cac5c8ed2bcfbbc7a9e8e62320b5ab98
Mirsaid <mirsaid.m@jafton.com>
2024-05-04 18:51:17 +0500

    done class work first

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 #1. Операторы больше и меньше в Python
 
-tst = int(input("Give me numbers?"))
+# tst = int(input("Give me numbers?"))
+# if tst > 0:
+#     print("True")
+# else:
+#     print("False")
+
+# class work
+
+tst = 5
+
 if tst > 0:
     print("True")
 else:
-    print("False")
-
+    print("FALSE")
